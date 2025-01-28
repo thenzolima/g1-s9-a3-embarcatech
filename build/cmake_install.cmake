0af328bca1ab0082b0885fbbf6b4f9bc39ba9d04
Rodrigo Dórea <rodrigodoreafelix@gmail.com>
2025-01-28 15:32:44 -0300

    Adicionando funcionalidade ao botão 6 do teclado matricial

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/enzo/.github/g1-s9-a3-embarcatech
+# Install script for directory: C:/Users/Rodrigo/Documents/EmbarcaTech/Unidade 4/Tarefa 2 - Unidade 4/g1-s9-a3-embarcatech
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/pio_matrix")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,12 +34,12 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/home/enzo/.pico-sdk/toolchain/13_3_Rel1/bin/arm-none-eabi-objdump")
+  set(CMAKE_OBJDUMP "C:/Users/Rodrigo/.pico-sdk/toolchain/13_3_Rel1/bin/arm-none-eabi-objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/enzo/.github/g1-s9-a3-embarcatech/build/pico-sdk/cmake_install.cmake")
+  include("C:/Users/Rodrigo/Documents/EmbarcaTech/Unidade 4/Tarefa 2 - Unidade 4/g1-s9-a3-embarcatech/build/pico-sdk/cmake_install.cmake")
 
 endif()
 
@@ -51,5 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/enzo/.github/g1-s9-a3-embarcatech/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Rodrigo/Documents/EmbarcaTech/Unidade 4/Tarefa 2 - Unidade 4/g1-s9-a3-embarcatech/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
